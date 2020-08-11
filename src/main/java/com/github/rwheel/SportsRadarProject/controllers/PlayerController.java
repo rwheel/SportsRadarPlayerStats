802265d7d30c94de6d25618d1dfd8b18ff551cb7
Ryan Wheeler <rwheeler@mastercontrol.com>
2020-08-10 23:54:19 -0600

    going to turn in

--- src/main/java/com/github/rwheel/SportsRadarProject/controllers/PlayerController.java
+++ src/main/java/com/github/rwheel/SportsRadarProject/controllers/PlayerController.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
@@ -21,31 +20,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 
 public class PlayerController implements Initializable {
     private String sportsRadarApiKey = System.getenv("Sports_API_Key");
     private String baseUrl = "http://api.sportradar.us/mlb/trial/v6.6/en";
     private HttpClient client;
     private static List<String> teamInfo;
-    private static List<String> teamName;
-    private static List<String> teamMarket;
-    private static List<String> teamID;
     private static List<String> playerInfo;
 
     @FXML
     private TextField teamNumberField;
-
     @FXML
     private TextArea outPutArea;
-
-    @FXML
-    private TextArea colOne;
-    @FXML
-    private TextArea colTwo;
-    @FXML
-    private TextArea colThree;
-
 
     @FXML
     void handleTeams(ActionEvent event) {
@@ -53,14 +40,12 @@ public class PlayerController implements Initializable {
         HashMap<String, String> params = new HashMap<>() {{
             put("api_key", sportsRadarApiKey);
         }};
-
         try {
             var response = doGet(uri, params);
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             var hierarchyResponse = gson.fromJson(response.body(), HierarchyResponse.class);
 
             displayTeamData(hierarchyResponse);
-
         } catch (Exception e) {
             System.out.println(e.toString());
         }
@@ -69,16 +54,15 @@ public class PlayerController implements Initializable {
     @FXML
     void handleTeamPlayers(ActionEvent event) {
         var teamID = teamNumberField.getText();
-        var uri = baseUrl + "/teams/"+teamID+"/profile.json";
-        HashMap<String, String> params = new HashMap<>(){{
-            put("api_key",sportsRadarApiKey);
+        var uri = baseUrl + "/teams/" + teamID + "/profile.json";
+        HashMap<String, String> params = new HashMap<>() {{
+            put("api_key", sportsRadarApiKey);
         }};
         try {
             var response = doGet(uri, params);
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             var team = gson.fromJson(response.body(), Team.class);
             displayPlayerData(team);
-
         } catch (Exception e) {
             System.out.println(e.toString());
         }
@@ -87,61 +71,56 @@ public class PlayerController implements Initializable {
 
     private HttpResponse<String> doGet(String uri, HashMap<String, String> params) throws Exception {
         var paramsString = params.keySet().stream()
-                                          .map((key)->key+"="+params.get(key))
-                                          .collect(Collectors.joining("&"));
-        var requestURI = URI.create(uri +"?"+paramsString);
+                                 .map((key) -> key + "=" + params.get(key))
+                                 .collect(Collectors.joining("&"));
+        var requestURI = URI.create(uri + "?" + paramsString);
 
         var request = HttpRequest.newBuilder(requestURI).GET().build();
 
         var response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         return response;
-
     }
+
     void displayTeamData(HierarchyResponse response) throws IOException {
-        String str = String.valueOf(response.getLeagues());
-        var into = str.replace("[", "")
+        String strA = String.valueOf(response.getLeagues());
+        var reformat = strA.replace("[", "")
                       .replace("]", "")
                       .replace("{", "")
                       .replace("}", "");
 
-        teamInfo = Arrays.stream(into.split("[,\\/]")).collect(Collectors.toList());
-        teamName = IntStream.range(0, teamInfo.size()).filter(n -> n % 3 == 0).mapToObj(teamInfo::get).collect(Collectors.toList());
-        teamMarket = IntStream.range(0, teamInfo.size()).filter(n -> n % 3 == 1).mapToObj(teamInfo::get).collect(Collectors.toList());
-        teamID = IntStream.range(0, teamInfo.size()).filter(n -> n % 3 == 2).mapToObj(teamInfo::get).collect(Collectors.toList());
-        System.out.print(into);
+        teamInfo = Arrays.stream(reformat.split("[,\\/]")).collect(Collectors.toList());
+     
         String strB = String.valueOf(teamInfo);
         var name = strB.replace("[", " ")
-                          .replace("]", "")
-                          .replace(", ", "\n");
+                       .replace("]", "")
+                       .replace(", ", "\n");
 
-         outPutArea.setText(name);
+        outPutArea.setText(" "+"MLB TEAMS\n\n"+name);
     }
 
 
     void displayPlayerData(Team response) throws IOException {
+        String strA = String.valueOf(response.getPlayers());
+        var reformat = strA.replace("[", "")
+                      .replace("]", "")
+                      .replace("{", "")
+                      .replace("}", "")
+                      .replace("null", "No College on Record");
 
-                String str = String.valueOf(response.getPlayers());
-                var into = str.replace("[", "")
-                              .replace("]", "")
-                              .replace("{", "")
-                              .replace("}", "")
-                              .replace("null", "No College on Record");
-                System.out.print(into);
-                playerInfo = Arrays.stream(into.split("[,\\/]")).collect(Collectors.toList());
+        playerInfo = Arrays.stream(reformat.split("[,\\/]")).collect(Collectors.toList());
 
         String strB = String.valueOf(playerInfo);
-
-             var name = strB.replace("[", " ")
+        var name = strB.replace("[", " ")
                        .replace("]", "")
                        .replace(", ", "\n");
 
-                outPutArea.setText(" "+response.getName()+"\n\n"+name);
-            }
+        outPutArea.setText(" " + response.getName() + " Players\n\n" + name);
+    }
 
-            @Override
-            public void initialize(URL url, ResourceBundle resourceBundle) {
-                client = HttpClient.newHttpClient();
-            }
-        }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        client = HttpClient.newHttpClient();
+    }
+}
 
